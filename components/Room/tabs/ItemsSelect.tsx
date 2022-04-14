f73aa6a9c574b1fabf68d666e6deb2bb8ad5963d
Andy Wang <climber2002@gmail.com>
2022-04-14 21:23:22 +1000

    keep image ratio

--- components/Room/tabs/ItemsSelect.tsx
+++ components/Room/tabs/ItemsSelect.tsx
@@ -1,4 +1,5 @@
 import { gql, useMutation, useQuery } from "@apollo/client";
+import { useState } from "react";
 import { Asset } from "../../../lib/types";
 import { transformAsset } from "../../../pages/main-room";
 import { createAsset, createAssetVariables } from "../../../__generated__/createAsset";
@@ -77,10 +78,17 @@ type ItemsSelectProps = {
   onAssetCreated: (asset: Asset) => any
 }
 
+type ImageSize = {
+  width: number;
+  height: number;
+}
+
 export function ItemsSelect({ roomId, onAssetCreated } : ItemsSelectProps) {
   const { data, loading, error } = useQuery<getOpenseaAssets, getOpenseaAssetsVariables>(GET_OPENSEA_ASSETS, {
     variables: { ownerAddress: '0x3DC22CB5a37C96d962194c9a0Cd8AFBA7710137C' }
   });
+
+  const [imageSizes, setImageSizes] = useState<Map<string, ImageSize>>(new Map<string, ImageSize>());
 
   const onCreateAssetCompleted = async ({ createAsset }: createAsset) => {
     const { ok, error, data } = createAsset;
@@ -113,12 +121,22 @@ export function ItemsSelect({ roomId, onAssetCreated } : ItemsSelectProps) {
                 <div className="aspect-w-1 aspect-h-1 rounded-lg bg-gray-100 overflow-hidden group-hover:opacity-75 mb-1">
                   <img src={asset.openseaImageThumbnailUrl} 
                     className="object-center object-cover"
+                    onLoad={(e) => {
+                      { /* keep image sizes so at server it can keep image ratio */ }
+                      setImageSizes(
+                        imageSizes.set(asset.openseaId, { width: (e.target as any).width, height: (e.target as any).height}))
+                    }}
+
                     onClick={() => {
+                      const imageSize = imageSizes.get(asset.openseaId);
+                      const width = imageSize?.width?? 100;
+                      const height = imageSize?.height?? 100;
+
                       createAsset({
                         variables: {
                           roomId,
-                          width: 100,
-                          height: 100,
+                          width: width,
+                          height: height,
                           openseaId: asset.openseaId,
                           openseaName: asset.openseaName,
                           openseaAssetContract: asset.openseaAssetContract,
@@ -140,7 +158,6 @@ export function ItemsSelect({ roomId, onAssetCreated } : ItemsSelectProps) {
 
       {
         createAssetLoading && 
-
         <div className="absolute top-0 left-0 w-full h-full z-50 overflow-hidden bg-gray-700 opacity-75 flex flex-col items-center justify-center">
           <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
           <h2 className="text-center text-white text-xl font-semibold">Adding Asset...</h2>

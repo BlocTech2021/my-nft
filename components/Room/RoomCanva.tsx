6f8fb470d8f228deaf13563415fcd8d0a60ef951
Andy Wang <climber2002@gmail.com>
2022-04-20 20:01:24 +1000

    use div for bg image

--- components/Room/RoomCanva.tsx
+++ components/Room/RoomCanva.tsx
@@ -1,7 +1,6 @@
-import { Dispatch, SetStateAction, useState } from "react";
-import { Stage, Layer, Circle, Image, Rect } from "react-konva";
-import useImage from "use-image";
-import { Color, getColorByName } from "../../lib/colors/color";
+import { Dispatch, SetStateAction } from "react";
+import { Stage, Layer } from "react-konva";
+import { getColorByName, hexToRgb } from "../../lib/colors/color";
 import { AssetEdit, Room } from "../../lib/types";
 import PhotoFrame from "../PhotoFrame";
 
@@ -15,54 +14,42 @@ export type RoomCanvaProps = {
 
 function RoomCanva({ room, editable, onAssetEdit, selectedAssetId, selectAssetWithId }: RoomCanvaProps) {
 
-  const [image] = useImage(room.backgroundImage?? '');
-
-  function renderBackground(room: Room) {
-    if (room.backgroundImage) {
-      return (
-        <Image image={image} width={window.innerWidth} height={window.innerHeight} listening={false} />
-      )
-    } else if (room.backgroundColor) {
-      const color = getColorByName(room.backgroundColor);
-      return (
-        <Rect width={window.innerWidth} height={window.innerHeight} listening={false} 
-            fillLinearGradientStartPoint={{x: 0, y: 0}}
-            fillLinearGradientEndPoint={{x: 0, y: window.innerHeight}}
-            fillLinearGradientColorStops={[
-              0,
-              color.startRgb,
-              1,
-              color.stopRgb,
-            ]} />
-      )
-    } 
-
+  let backgroundStyle = {};
+  if (room.backgroundImage) {
+    backgroundStyle = {
+      backgroundImage: `url(${room.backgroundImage})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+    }
+  } else if (room.backgroundColor) {
+    const color = getColorByName(room.backgroundColor);
+    backgroundStyle = {
+      backgroundImage: `linear-gradient(${hexToRgb(color.startRgb)}, ${hexToRgb(color.stopRgb)})`,
+    }
   }
 
   return (
-    <Stage width={window.innerWidth} height={window.innerHeight} onClick={() => {
-      if(!selectAssetWithId) {
-        return;
-      }
-      selectAssetWithId(undefined) 
-    }}>
-      <Layer>
-        { renderBackground(room) }
-      </Layer>
-      <Layer offsetX={-window.innerWidth / 2} offsetY={-window.innerHeight / 2}>
-        {/* <PhotoFrame imgSrc={"/nfts/cat.jpeg"} width={200} height={200} x={100} y={200} frame={Frame.Frame1} />
-        <PhotoFrame imgSrc={"/nfts/monkey.jpeg"} width={200} height={300} x={320} y={200} frame={Frame.Frame2} /> */}
-
-        {
-          room.assets.map(asset => (
-            <PhotoFrame key={asset.id} asset={asset} 
-              isSelected={asset.id === selectedAssetId}
-              selectAssetWithId={editable ? selectAssetWithId : undefined}
-              onAssetEdit={editable ? onAssetEdit : undefined} />
-          ))
+    <div style={backgroundStyle}>
+      <Stage width={window.innerWidth} height={window.innerHeight} onClick={() => {
+        if(!selectAssetWithId) {
+          return;
         }
-      </Layer>
-    </Stage>
+        selectAssetWithId(undefined) 
+      }}>
+        {/* put origin as center of screen */}
+        <Layer offsetX={-window.innerWidth / 2} offsetY={-window.innerHeight / 2}> 
+          {
+            room.assets.map(asset => (
+              <PhotoFrame key={asset.id} asset={asset} 
+                isSelected={asset.id === selectedAssetId}
+                selectAssetWithId={editable ? selectAssetWithId : undefined}
+                onAssetEdit={editable ? onAssetEdit : undefined} />
+            ))
+          }
+        </Layer>
+      </Stage>
+    </div>
   );
 }
 
--- lib/colors/color.ts
+++ lib/colors/color.ts
@@ -56,3 +56,13 @@ export function getColorByName(name: string): Color {
   const color = allColors.find(color => color.name === name);
   return color ? color : allColors[0];
 }
+
+
+export function hexToRgb(hex: string) {
+  let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  if (!result) {
+    return 'rgb(255, 255, 255)';
+  }
+
+  return `rgb(${ parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)})`;
+}
